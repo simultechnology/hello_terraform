de952022acd97950e34950f006c163386d74134b
t_ishikawa <tissi0708@gmail.com>
2022-02-11 15:14:26 +0900

    update

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "hello" {
+  ami           = "ami-0701e21c502689c31"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "hello"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
